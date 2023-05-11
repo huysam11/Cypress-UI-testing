cdfa44ebbc67ae03ed350d8b074bafa2af2ca19b
Huy Sam <huyprolove114@gmail.com>
2023-05-11 18:56:44 -0400

    SauceDemoTest

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
       viewportWidth: 1500,
       viewportHeight: 900,
   e2e: {
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
