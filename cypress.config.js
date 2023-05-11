d82af2ad7afc5f05e0bbf6258f0e4e32043cbfef
Huy Sam <huyprolove114@gmail.com>
2023-05-11 16:41:00 -0400

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+module.exports = defineConfig({
+      viewportWidth: 1500,
+      viewportHeight: 900,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
